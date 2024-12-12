2e467cdcd8d75ad3bd652afc6e0b53c3bce2d161
Gerson121295 <gerson21millones@gmail.com>
2024-12-12 11:47:22 -0600

    S4-Rutas V7 tradicionales sin LazyLoad

---
+++ 02-react-app/src/01-lazyload/pages/index.ts
@@ -0,0 +1,9 @@
+//archivo index.ts permite exportar todos los archivos de carpeta pages para evitar importar cada uno en el archivo a usar
+
+export { LazyPage1 } from './LazyPage1';
+export { LazyPage2 } from './LazyPage2';
+export { LazyPage3 } from './LazyPage3';
+
+
+
+
---
+++ 02-react-app/src/routes/Navigation.tsx
@@ -0,0 +1,44 @@
+import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router"
+
+import logo from '../assets/react.svg'
+
+//debido a que dentro de carpeta pages se encuentra el archivo index.ts el cual exporta todos los archivos de carpeta pages por lo que se puede seleccionar los archivos a importar
+import {  LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages"
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      
+      <div className="main-layout">
+        <nav>
+            <img src={logo} alt="React Logo" />
+            <ul>
+                <li>
+                    {/* desestructura isActive y valida si es true le agrega el estilo CSS 'nav-active' sino no agrega nada, regresa un string vacio '' */}
+                    <NavLink to={"/lazy1"} className={ ({isActive}) => isActive ? 'nav-active' : '' }>Lazy 1</NavLink>
+                </li>
+                <li>
+                    <NavLink to={"/lazy2"} className={ ({isActive}) => isActive ? 'nav-active' : '' }>Lazy 2</NavLink>
+                </li>
+                <li>
+                    <NavLink to={"/lazy3"} className={ ({isActive}) => isActive ? 'nav-active' : '' }>Lazy 3</NavLink>
+                </li>
+            </ul>
+        </nav>
+    
+    
+        <Routes>
+            {/* <Route path="/" element={<App />} /> */}
+            <Route path="lazy1" element={ <LazyPage1 /> } />  {/* <h1>lazy1</h1> */}
+            <Route path="lazy2" element={ <LazyPage2 /> } />
+            <Route path="lazy3" element={ <LazyPage3 /> } />
+
+            {/* //Si la ruta ingresada no existe redirija al home, y con replace definimos que no pueda regresar */}
+            <Route path="/*" element={ <Navigate to="/lazy1" replace/> } />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
+  )
+}
+
